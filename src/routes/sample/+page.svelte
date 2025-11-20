186b654a17eb594832b4d29666fadaf8e5f3ee14
Artemio <artem.ustimenko2008@gmail.com>
2025-11-20 15:56:02 +0100

    made redirect on /sample async

--- src/routes/sample/+page.svelte
+++ src/routes/sample/+page.svelte
@@ -2,29 +2,21 @@
   import { goto } from '$app/navigation';
   import { onMount } from 'svelte';
 
-  onMount(async () => {
-    const searchParams = new URLSearchParams(window.location.search);
-    const id = searchParams?.get('id') || '';
-
-    try {
-      const response = await fetch('/api/write-analytics', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json', 'k': import.meta.env.VITE_MY_API_KEY },
-        body: JSON.stringify({
-          id: id,
-          browser: navigator.userAgent,
-          os: navigator.platform,
-          language: navigator.language,
-          viewport_type: window.innerWidth >= window.innerHeight ? 'landscape' : 'portrait',
-          referrer: document.referrer,
-        })
-      });
-      if (!response.ok) {
-        console.error("Something went wrong.");
-      }
-    } catch (e) {
-      console.error("Something went wrong. \n", e);
-    }
+  onMount(() => {
+    const id = new URLSearchParams(window.location.search)?.get('id') || '';
+    fetch('/api/write-analytics', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json', 'k': import.meta.env.VITE_MY_API_KEY },
+      body: JSON.stringify({
+        id: id,
+        browser: navigator.userAgent,
+        os: navigator.platform,
+        language: navigator.language,
+        viewport_type: window.innerWidth >= window.innerHeight ? 'landscape' : 'portrait',
+        referrer: document.referrer,
+      })
+    }).catch((e) => console.error("Analytics error:", e));
+    
     goto('/');
   });
 </script>
