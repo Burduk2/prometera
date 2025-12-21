3d40e0750f960c179bb359d009e7f135b656fc75
Artemio <artem.ustimenko2008@gmail.com>
2025-12-21 16:40:05 +0100

    add more precise refer tracking

--- src/routes/[...catchall]/+page.svelte
+++ src/routes/[...catchall]/+page.svelte
@@ -5,8 +5,9 @@
   
   export let data;
   onMount(() => { 
+    const id = new URLSearchParams(window.location.search)?.get('id') || '';
     history.replaceState(null, '', '/');
-    if (data.ref) submitAction('', null, data.ref, { action_details: data.ref }) 
+    if (data.ref) submitAction('', null, data.ref, { action_details: id }); 
   });
 </script>
 
